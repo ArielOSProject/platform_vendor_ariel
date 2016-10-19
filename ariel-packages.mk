fff2345434b1c26cd84023baed609af371a8afab
Sava Mikalacki <mikalackis@gmail.com>
2016-10-19 22:25:19 +0200

    Added fixes for accessing data/system/ifw
    init.ariel.rc
    Used to create the ifw directory with appropriate
    permissions for system user.
    
    sepolicy files
    Create a sepparate label for /data/system/ifw and
    grant system_app write and read permissions so the app
    can write firewall rules

--- ariel-packages.mk
+++ ariel-packages.mk
@@ -1,12 +1,21 @@
 $(call inherit-product, vendor/ariel/ariel-sdk.mk)
+$(call inherit-product, vendor/ariel/sepolicy/sepolicy.mk)
 
 PRODUCT_PACKAGES += \
    ArielGuardian \
-   Eleven \
+#   Eleven \
    CMFileManager \
    ArielSettingsProvider
 
 # Make sure data roaming is off!
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.android.dataroaming=false
+
+# Copy IntentFirewall configuration file
+#PRODUCT_COPY_FILES += \
+#    vendor/ariel/firewall/ifw.config:data/system/ifw/ifw.config
+
+# Copy .rc files
+PRODUCT_COPY_FILES += \
+    vendor/ariel/init.ariel.rc:root/init.ariel.rc
 
---
+++ sepolicy/sepolicy.mk
@@ -0,0 +1,8 @@
+
+#
+# This policy configuration will be used by all products that
+# inherit from Ariel
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/ariel/sepolicy
