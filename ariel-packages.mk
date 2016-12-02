bd50abce4811eed6e41df0ba122e94cdb74ddf17
Sava Mikalacki <mikalackis@gmail.com>
2016-12-02 12:03:34 +0100

    Integrated sdk with vendor setup

--- ariel-packages.mk
+++ ariel-packages.mk
@@ -1,11 +1,16 @@
 
-include vendor/ariel/platform/ariel-platform.mk
+#include vendor/ariel/platform/ariel-platform.mk
 include vendor/ariel/sepolicy/sepolicy.mk
 
+# Ariel Platform Library
+PRODUCT_PACKAGES += \
+    com.ariel.platform-res \
+    com.ariel.platform \
+    com.ariel.platform.xml
 
+#Eleven
 PRODUCT_PACKAGES += \
    ArielGuardian \
-#   Eleven \
    CMFileManager \
    ArielSettingsProvider
 
@@ -20,6 +25,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Copy .rc files
 PRODUCT_COPY_FILES += \
     vendor/ariel/init.ariel.rc:root/init.ariel.rc
+
+include $(call first-makefiles-under,$(LOCAL_PATH))
 
 # mozda probati i sa include <Putanja do mk fajla>
 
