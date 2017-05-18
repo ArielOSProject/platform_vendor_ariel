c104b11f88282f10f78d9901f69f5bb767b49553
Sava Mikalacki <mikalackis@gmail.com>
2017-05-18 11:05:02 +0200

    Fixed conflicts

--- platform/ariel/lib/main/java/com/ariel/platform/internal/ArielSystemServer.java
+++ platform/ariel/lib/main/java/com/ariel/platform/internal/ArielSystemServer.java
@@ -1,4 +1,36 @@
 /**
+ * Created by mikalackis on 7.3.17..
+ * <p>
+ * Copyright (C) 2016 The CyanogenMod Project
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * <p>
+ * Copyright (C) 2016 The CyanogenMod Project
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
  * Copyright (C) 2016 The CyanogenMod Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -19,10 +51,13 @@ package com.ariel.platform.internal;
 import android.content.Context;
 import android.os.SystemProperties;
 import android.util.Slog;
+
 import com.android.server.LocalServices;
 import com.android.server.SystemServiceManager;
 import com.android.server.SystemConfig;
+
 import android.util.ArraySet;
+
 import java.util.Iterator;
 
 import android.os.IDeviceIdleController;
@@ -62,7 +97,7 @@ public class ArielSystemServer {
         // Only run "core" apps+services if we're encrypting the device.
         final String cryptState = SystemProperties.get("vold.decrypt");
         return ENCRYPTING_STATE.equals(cryptState) ||
-               ENCRYPTED_STATE.equals(cryptState);
+                ENCRYPTED_STATE.equals(cryptState);
     }
 
     /**
@@ -73,49 +108,17 @@ public class ArielSystemServer {
         try {
             Slog.i(TAG, "ArielSystemServer starting services...");
             try {
-                            IBackupManager ibm = IBackupManager.Stub.asInterface(
-                                    ServiceManager.getService(Context.BACKUP_SERVICE));
-                            ibm.setBackupServiceActive(UserHandle.USER_OWNER, true);
-                        } catch (RemoteException e) {
-                            throw new IllegalStateException("Failed activating backup service.", e);
-                        }
+                IBackupManager ibm = IBackupManager.Stub.asInterface(
+                        ServiceManager.getService(Context.BACKUP_SERVICE));
+                ibm.setBackupServiceActive(UserHandle.USER_OWNER, true);
+            } catch (RemoteException e) {
+                throw new IllegalStateException("Failed activating backup service.", e);
+            }
             startServices();
-            SystemConfig sysConfig = SystemConfig.getInstance();
-            ArraySet<String> allowPower = sysConfig.getAllowInPowerSave();
-            if(allowPower!=null && allowPower.size()>0){
-                Slog.i(TAG, "Power save enabled apps:");
-                Iterator<String> it = allowPower.iterator();
-                while(it.hasNext()){
-                    String packageName = it.next();
-                    Slog.i(TAG, "Power save enabled package: "+packageName);
-                }
-            }
-            else{
-                Slog.i(TAG, "No power save enabled apps.");
-            }
-            configureGuardianApp();
         } catch (Throwable ex) {
             Slog.e("System", "******************************************");
             Slog.e("System", "************ Failure starting cm system services", ex);
             throw ex;
-        }
-    }
-
-    private void configureGuardianApp(){
-        mDeviceIdleService = IDeviceIdleController.Stub.asInterface(
-                        ServiceManager.getService(DEVICE_IDLE_SERVICE));
-
-        PowerManager power = mSystemContext.getSystemService(PowerManager.class);
-        if (power.isIgnoringBatteryOptimizations("com.ariel.guardian")) {
-            Slog.i(TAG, "ArielGuardian is already ignoring battery optimizations...");
-            return;
-        }
-
-        try {
-            mDeviceIdleService.addPowerSaveWhitelistApp("com.ariel.guardian");
-            Slog.i(TAG, "ArielGuardian is now ignoring battery optimizations...");
-        } catch (RemoteException e) {
-            Slog.w(TAG, "Unable to reach IDeviceIdleController", e);
         }
     }
 
@@ -128,7 +131,7 @@ public class ArielSystemServer {
         for (String service : externalServices) {
             try {
                 Slog.i(TAG, "Attempting to start service " + service);
-                ArielSystemService arielSystemService =  mSystemServiceHelper.getServiceFor(service);
+                ArielSystemService arielSystemService = mSystemServiceHelper.getServiceFor(service);
                 if (context.getPackageManager().hasSystemFeature(
                         arielSystemService.getFeatureDeclaration())) {
                     if (coreAppsOnly() && !arielSystemService.isCoreService()) {
@@ -143,7 +146,7 @@ public class ArielSystemServer {
                             " due to feature not declared on device");
                 }
             } catch (Throwable e) {
-                reportWtf("starting " + service , e);
+                reportWtf("starting " + service, e);
             }
         }
 
@@ -154,4 +157,5 @@ public class ArielSystemServer {
         Slog.w(TAG, "***********************************************");
         Slog.wtf(TAG, "BOOT FAILURE " + msg, e);
     }
+
 }
