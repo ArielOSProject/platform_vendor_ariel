ebcd9b1b06a629d190cc351a0ec3600bc994d715
Sava Mikalacki <mikalackis@gmail.com>
2018-10-05 09:42:19 +0200

    Rework sepolicy, to be moved to device_ariel_sepolicy at some point

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -4,5 +4,10 @@
 # inherit from Ariel
 #
 
-BOARD_SEPOLICY_DIRS += \
-    vendor/ariel/sepolicy
+# BOARD_SEPOLICY_DIRS += \
+#     vendor/ariel/sepolicy
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    vendor/ariel/sepolicy/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    vendor/ariel/sepolicy/private
