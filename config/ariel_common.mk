66201ef0aca3f3b8cbbd060815bc78203a5d5b56
mikalackis <mikalackis@gmail.com>
2019-03-06 21:09:58 +0100

    Remove unused hosts file

--- config/ariel_common.mk
+++ config/ariel_common.mk
@@ -7,8 +7,7 @@ PRODUCT_COPY_FILES += \
     vendor/ariel/prebuilt/common/etc/permissions/com.arielos.firewall.xml:system/etc/permissions/com.arielos.firewall.xml \
     vendor/ariel/prebuilt/common/etc/permissions/com.arielos.security.xml:system/etc/permissions/com.arielos.security.xml \
     vendor/ariel/prebuilt/common/etc/permissions/com.arielos.adblock.xml:system/etc/permissions/com.arielos.adblock.xml \
-    vendor/ariel/prebuilt/common/etc/sysconfig/ariel-sysconfig.xml:system/etc/sysconfig/ariel-sysconfig.xml \
-    vendor/ariel/prebuilt/common/etc/hosts.ariel:system/etc/hosts.ariel
+    vendor/ariel/prebuilt/common/etc/sysconfig/ariel-sysconfig.xml:system/etc/sysconfig/ariel-sysconfig.xml
 
 
 # Copy .rc files
