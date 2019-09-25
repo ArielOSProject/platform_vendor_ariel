5d50534910ecf3d7d0976bc8d0c178990eabafd6
Sava Mikalacki <sava@codekrafters.com>
2019-09-25 09:28:57 +0200

    Support default ariel revision

--- build/tools/roomservice.py
+++ build/tools/roomservice.py
@@ -113,7 +113,7 @@ def indent(elem, level=0):
 
 def get_default_revision():
     m = ElementTree.parse(".repo/manifest.xml")
-    d = m.findall('default')[0]
+    d = m.findall('default-ariel')[0]
     r = d.get('revision')
     return r.replace('refs/heads/', '').replace('refs/tags/', '')
 
