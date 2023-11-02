e86eacc89fd27a9274f4b18627d409d284f90227
Sava Mikalacki <mikalackis@gmail.com>
2023-11-02 09:40:01 +0100

    Update hosts file, remove unused permissions and exceptions

--- config/ariel_packages.mk
+++ config/ariel_packages.mk
@@ -1,18 +1,11 @@
 PRODUCT_PACKAGES += \
   AuroraStore \
-  DejaVu \
   GmsCore \
   GsfProxy \
   FakeStore \
-  IchnaeaNlpBackend \
-  NominatimGeocoderBackend \
   ArielGuardian \
   NewPipe
-  #NetworkLocation -> this is now part of gms core from microg
-  #com.google.android.maps.jar \
-
 
 # ArielGuardian
 #  arielfw - excluded until we create a system service with daemon connector
 #            check: https://github.com/ArielOSProject/platform_vendor_ariel/blob/master_ditch_shared_system_id/platform/ariel/lib/main/java/com/ariel/platform/internal/firewall/ArielFirewallService.java
-  
