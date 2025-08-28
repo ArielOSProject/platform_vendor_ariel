bbe8b818d73ecb2c4bff304ba1b719813d47a0ba
Sava Mikalacki <mikalackis@gmail.com>
2025-08-28 15:22:44 +0200

    Update fake store to microg vending

--- config/ariel_packages.mk
+++ config/ariel_packages.mk
@@ -2,14 +2,14 @@ PRODUCT_PACKAGES += \
   AuroraStore \
   GmsCore \
   GsfProxy \
-  FakeStore \
+  MicrogVending \
   ArielGuardian \
   NewPipe \
   ArielParental \
-  GoogleContactsSyncAdapter \
   GoogleCalendarSyncAdapter \
   ArielProtector
 
 # ArielGuardian
+#FakeStore \
 #  arielfw - excluded until we create a system service with daemon connector
 #            check: https://github.com/ArielOSProject/platform_vendor_ariel/blob/master_ditch_shared_system_id/platform/ariel/lib/main/java/com/ariel/platform/internal/firewall/ArielFirewallService.java
