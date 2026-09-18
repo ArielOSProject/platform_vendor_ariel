eda8b25474ce1f87ae4d86d3970977d386734f9d
Sava Mikalacki <mikalackis@gmail.com>
2026-09-18 13:54:06 +0200

    Remove release build modifications, include SkyTube and DuckDuckGo

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -104,7 +104,7 @@ function arielbuild() {
         local fp="${ANDROID_PRODUCT_OUT}/build_fingerprint.txt"
         echo "${BUILD_NUMBER} $(date -u +%FT%TZ) ${TARGET_PRODUCT:-$1} $(cat "$fp" 2>/dev/null)" \
             >> "$(gettop)/vendor/ariel/build-history.txt"
-        echo "Upisano u build-history.txt"
+        echo "ArielOS build-history.txt updated!"
     fi
     return $rc
 }
